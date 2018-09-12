92e9a60cb9f2944814467381e1a6a9a3d053bdd7
xjm <13637289523@163.com>
2018-09-12 18:22:09 +0800

    xjm

--- pinyougou_parent/pinyougou_cart_interface/src/main/java/com/pinyougou/cart/service/CartService.java
+++ pinyougou_parent/pinyougou_cart_interface/src/main/java/com/pinyougou/cart/service/CartService.java
@@ -3,6 +3,7 @@ package com.pinyougou.cart.service;
 import java.util.List;
 
 import groupEntity.Cart;
+import groupEntity.CartVo;
 
 public interface CartService {
 
@@ -15,5 +16,8 @@ public interface CartService {
 	List<Cart> mergeCartList(List<Cart> cartList_session, List<Cart> cartList_userId);
 
 	void clearRedisByKey(String sessionId);
+    
+	public List<Cart> addItemToCartList(List<Cart> cartList,Long itemId,Integer num );
 
+	List<Cart> mergeLocalStrageAndRedis(String userId, CartVo vo);
 }
